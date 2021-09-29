736393aaaab84fdfe94bd18decde481ffca86b9b
battuvijaysai <79572195+battuvijaysai@users.noreply.github.com>
2021-09-29 15:32:57 +0530

    Create Example.java

---
+++ Example.java
@@ -0,0 +1,16 @@
+package MyPack;
+class Example
+{
+int a;
+void display()
+{
+System.out.println("Hello");
+}
+public static void main(String args[])
+{
+Example Ex=new Example();
+A ob=new A();
+Ex.display();
+ob.show();
+}
+}
